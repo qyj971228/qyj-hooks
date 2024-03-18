aaf9709c6131a4e26927d4bfe5c1072f39a228ae
qyj <1078869122@qq.com>
2024-03-18 14:31:17 +0800

    feat: useAroundExecution error catch

--- src/hooks/useAroundExecution/index.ts
+++ src/hooks/useAroundExecution/index.ts
@@ -1,25 +1,32 @@
-import { watch } from "vue"
-import { useOpposite } from "@/hooks"
+import { watch } from "vue";
+import { useOpposite } from "@/hooks";
 
-export function useAroundExecution(asyncCallback: () => Promise<void>, before: () => void, after: () => void) {
-  const { state, recover: endFetch, reverse: startFetch } = useOpposite()
+export function useAroundExecution(
+  asyncCallback: () => Promise<void>,
+  before: () => void,
+  after: () => void
+) {
+  const { state, recover: endFetch, reverse: startFetch } = useOpposite();
 
   watch(state, (val) => {
     if (val) {
-      before()
+      before();
     } else {
-      after()
+      after();
     }
-  })
+  });
 
   async function loader() {
-    if (state.value) return
-    startFetch()
-    await asyncCallback()
-    endFetch()
+    if (state.value) return;
+    startFetch();
+    try {
+      await asyncCallback();
+    } finally {
+      endFetch();
+    }
   }
 
   return {
-    loader
-  }
+    loader,
+  };
 }
