ea51073382065fa19e05ddc718993e30ed8428a4
qyj <1078869122@qq.com>
2024-06-18 11:32:52 +0800

    fix: useOpp Function Overload Signatures

--- src/hooks/useOpposite/index.ts
+++ src/hooks/useOpposite/index.ts
@@ -1,33 +1,37 @@
-import { computed, ref, type ComputedRef } from "vue";
+import { ref, computed, type ComputedRef } from 'vue'
 
-type UseOppReturnType<T, K> = [
-  T extends unknown ? ComputedRef<boolean> : ComputedRef<boolean | T | K>,
-  () => void,
-  () => void,
-  () => void
-];
+// 重载签名
+export function useOpp<T, K>(status: [T, K]): [ComputedRef<T | K>, () => void, () => void, () => void]
+export function useOpp(): [ComputedRef<boolean>, () => void, () => void, () => void]
 
-export function useOpp<T, K>(status?: [T, K]): UseOppReturnType<T, K> {
-  const DEFAULT = false;
-  const OPPOSITE = true;
-  const state = ref(DEFAULT);
+// 实现签名
+export function useOpp<T, K>(status?: [T, K]) {
+  const DEFAULT = false
+  const OPPOSITE = true
+  const state = ref(DEFAULT)
 
   function opposite() {
-    state.value = state.value === DEFAULT ? OPPOSITE : DEFAULT;
+    state.value = state.value === DEFAULT ? OPPOSITE : DEFAULT
   }
+
   function recover() {
-    state.value = DEFAULT;
+    state.value = DEFAULT
   }
+
   function reverse() {
-    state.value = OPPOSITE;
+    state.value = OPPOSITE
   }
 
   const resultState = computed(() => {
     if (status) {
       return state.value === DEFAULT ? status[0] : status[1]
+    } else {
+      return state.value
     }
-    return state.value
-  });
+  })
 
-  return [resultState as any, recover, reverse, opposite] as any;
+  return [resultState, recover, reverse, opposite]
 }
+
+// useOpp()[0]
+// useOpp([Symbol, new Date()])[0]
